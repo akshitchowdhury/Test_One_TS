30515d6fa20a3f499e9fa25ed24a32d832b4fa7b
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-12-03 23:32:49 +0530

    PropType2

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 
 import './App.css'
 import {FC} from 'react'
+import Home from './component/Home/Home';
 
-interface AppProps {
-  title: String;
+// interface AppProps {
+//   title: String;
+// }
+
+type AppProps = {
+  title: string;
+  test_one: string;
 }
-const App: FC<AppProps> = ()=> {
+const App: FC<AppProps> = ({title,test_one})=> {
 
   return (
     
     <>
     <p>App</p>
+    <h1>Prop type test: {title}</h1>
+    <h2>{test_one}</h2>
+    <Home/>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,6 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App title="Vite + React + TS" />
+    <App title="Vite + React + TS" test_one='testing'/>
   </StrictMode>,
 )
