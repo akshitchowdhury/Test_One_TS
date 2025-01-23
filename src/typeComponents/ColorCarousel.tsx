447bd597b0ba91e3d9058f6160ed63800cb7d0d7
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2025-01-23 22:49:20 +0530

    ColorBox in ts2

--- src/typeComponents/ColorCarousel.tsx
+++ src/typeComponents/ColorCarousel.tsx
@@ -3,14 +3,31 @@ import React, { useState } from 'react'
 const ColorCarousel : React.FC=() => {
     
     const colors = ['red','violet','yellow','pink']
-    const[color,setColor] = useState(colors[0])
+    const[colorIndex,setColorIndex] = useState(0)
+    const[color,setColor] = useState(colors[colorIndex])
+    
     return (
         <>
-    <div style={{width: "400px", height: "600px",
-        border: "2pt solid black"
+    <div style={{width: "400px", height: "500px",
+        border: "2pt solid black",
+        backgroundColor: color
     }}>
       
     </div>
+<div style={{display: "flex", flexDirection:'row', gap:"200px"}}>
+    <button  style={{backgroundColor: 'red'}}>
+        Prev Color
+    </button>
+    <button 
+    onClick={()=> 
+        {
+            setColorIndex(colorIndex+1)
+        setColor(colors[colorIndex])
+    }}
+    style={{backgroundColor: 'blue'}}>
+        Next Color
+    </button>
+  </div>  
     </>
   )
 }
